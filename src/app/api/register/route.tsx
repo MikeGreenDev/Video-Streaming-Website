a9aca0668c9ccee4b9572ae0b7d8e1b1a76af72f
MikeGreenDev <mdgreen155@yahoo.com>
2024-11-11 12:15:08 -0500

    Fixed: Register duplicate Error messages

--- src/app/api/register/route.tsx
+++ src/app/api/register/route.tsx
@@ -6,7 +6,9 @@ import { Prisma } from "@prisma/client";
 
 export async function POST(request: Request) {
     const body = await request.json();
-    const { email, username, password } = body;
+    let { email, username, password }: {email: string, username: string, password: string} = body;
+    email = email.toLowerCase();
+    username = username.toLowerCase();
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
@@ -15,22 +17,22 @@ export async function POST(request: Request) {
             data: {
                 email,
                 username,
+                displayName: username,
                 passwordHash: hashedPassword,
             },
         });
         return NextResponse.json(user);
     } catch (e: any) {
-        console.log("ERROR")
-        console.log(e)
         if (e instanceof Prisma.PrismaClientKnownRequestError){
             if (e.code === "P2002") {
-                if (e.meta?.target == "User_name_key") {
-                    console.log("USER ERROR")
-                    return NextResponse.json({error: "User Already Taken"}, {status: 500});
+                if (e.meta?.target[0] == "username") {
+                    return NextResponse.json({error: "Username Already Taken"}, {status: 500});
+                }
+                if (e.meta?.target[0] == "email") {
+                    return NextResponse.json({error: "Email Already Taken"}, {status: 500});
                 }
             }
         }
+        return NextResponse.json({error: "Unknown Error"}, {status: 500});
     }
-
-
 }
